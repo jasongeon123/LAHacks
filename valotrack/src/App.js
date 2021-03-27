fe6768d7052a41bb7097a7d1d0a3b3168d2c3950
Geon Yoo <46584775+jasongeon123@users.noreply.github.com>
2021-03-27 15:15:54 -0700

    upload functino

--- valotrack/src/App.js
+++ valotrack/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { Component } from 'react';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import './App.css';
+ 
+class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        selectedFile: null
+      }
+   
+  };
+ 
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+            <ToastContainer />
+            <form method="post" action="#" id="#">
+                  <div className="form-group files">
+                    <input type="file" name="file" className="form-control" onChange={this.onChangeHandler}/>
+                  </div>
+                  <div className="col-md-6 pull-right">
+                    <button width="100%" type="button" className="btn btn-info" onClick={this.fileUploadHandler}>Upload File</button>
+                  </div>
+              </form>
+        </div>
+      </div>
     </div>
   );
+}
+  onChangeHandler=event=>{
+    var file = event.target.files[0];
+    if(this.validateSize(event)){ 
+  // if return true allow to setState
+     this.setState({
+      selectedFile: file
+      });
+ 
+    }
+  }
+  fileUploadHandler = () => {
+    const data = new FormData()
+    data.append('file', this.state.selectedFile)
+    axios.post("http://localhost:3000", data)
+      .then(res => { // then print response status
+        toast.success('upload success')
+      })
+      .catch(err => { // then print response status
+        toast.error('upload fail')
+      })
+ 
+  };
+  validateSize=(event)=>{
+      let file = event.target.files[0];
+      let size = 1000000000;
+      let err = '';
+      if (file.size > size) {
+      err = 'is too large, please pick a smaller file\n';
+      toast.error(err);
+      }
+    return true
+  };
 }
 
 export default App;
